2c4256a48354813bc44a8772849df79ca2253738
Mind <kartik.ayikkathil@mindinventory.com>
2024-09-12 15:46:17 +0530

    fix code

--- controllers/start_detection.js
+++ controllers/start_detection.js
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
     try {
         // Extract S3 bucket and video key from the event
         const bucket = 'project-videostore';
-        const key = 's3://project-videostore/potrait_sample.mp4';
+        const key = 'potrait_sample.mp4';
 
         // Parameters for the Rekognition video job
         const params = {
