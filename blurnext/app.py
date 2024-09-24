5134e94fcc3465a9b5b6abb4d7fb4c333dc0ee70
Mind <kartik.ayikkathil@mindinventory.com>
2024-09-24 15:43:21 +0530

    Try another vid

--- blurnext/app.py
+++ blurnext/app.py
@@ -84,7 +84,7 @@ def lambda_function(event, context):
         print(timestamps)
         # get metadata of file uploaded to Amazon S3
         bucket = 'project-videostore'
-        key = 'walking_speed.mp4'
+        key = 'walking_sp_blur.mp4'
         filename = key
         local_filename = '/tmp/{}'.format(filename)
         local_filename_output = '/tmp/anonymized-{}'.format(filename)
