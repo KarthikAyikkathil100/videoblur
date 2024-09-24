6dafbe6cdd44253b8595a3c984cf43e5a18416da
Mind <kartik.ayikkathil@mindinventory.com>
2024-09-24 16:50:33 +0530

    try airport vid

--- controllers/start_detection.js
+++ controllers/start_detection.js
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
     try {
         // Extract S3 bucket and video key from the event
         const bucket = 'project-videostore';
-        const key = 'walking_speed_format.mov';
+        const key = 'airport.mp4';
 
         // Parameters for the Rekognition video job
         const params = {
